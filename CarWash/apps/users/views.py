f2b840a5156e94f3994b7e278f4c814333d10406
CREESTL <kroymw3@yandex.ru>
2020-03-31 18:23:11 +0300

    Сделал добавление пользователя в FireBase через register

--- CarWash/apps/users/views.py
+++ CarWash/apps/users/views.py
@@ -4,35 +4,48 @@ from django.conf import settings
 from django.shortcuts import render, redirect, get_object_or_404
 import os
 import fnmatch
+import pyrebase
 
 # ОБЯЗАТЕЛЬНО СПЕРЕДИ ПИСАТЬ users !!!
 from users.models import Single_User
 from users.models import Video
 from users.forms import UserRegisterForm # кастомная форма регистрации
 
+
+
+#######################################################################################
+# конфиг для FireBase
+config = {
+    'apiKey': "AIzaSyBnsFHlZ9IP7kATKMILibYxT4y2_CRWUrM",
+    'authDomain': "carwash-838f3.firebaseapp.com",
+    'databaseURL': "https://carwash-838f3.firebaseio.com",
+    'projectId': "carwash-838f3",
+    'storageBucket': "carwash-838f3.appspot.com",
+    'messagingSenderId': "548753653087",
+    'appId': "1:548753653087:web:1dcba0736eff005002caea",
+    'measurementId': "G-7YLLEJ4SX7"
+  }
+
+firebase = pyrebase.initialize_app(config)
+
+auth = firebase.auth()
+######################################################################################
 # Это вьюшка для страницы регистрации.
 def register(request):
     if request.method == 'POST': # Если был создан POST запрос на регистрацию, то форма создается со всеми данными запроса
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            # СОХРАНЕНИЕ ПОЛЬЗОВАТЕЛЯ
-            form.save()
+        email = request.POST.get('email') # эти поля взяты из forms.py
+        print("\n\n\n email is " + email + '\n\n\n')
+        password = request.POST.get('password1')
 
-            username = form.cleaned_data.get('username')
-            # Бывают такие flash-сообщения
-            # messages.debug
-            # messages.info
-            # messages.success
-            # messages.warning
-            # messages.error
-            messages.success(request, f'Аккаунт создан!')
-            return redirect('../login')
-        else:
-            print("Form is not valid!")
+        # Создается пользователь и сохраняется в FireBase
+        user = auth.create_user_with_email_and_password(email, password)
+
+        messages.success(request, f'Аккаунт создан!')
+        return redirect('../login')
     else:
-        form = UserRegisterForm() # Если нет, то создается просто пустая форма
-        print(1)
-    return render(request, 'users/register.html', {'form': form})
+        print("Form is not valid!")
+   
+    return render(request, 'users/register.html')
 
 # Домашняя страница
 def index(request):
